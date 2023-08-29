95f668e612e23a31acd3d9ecf2908b2a8a21f877
Ade-nike17 <perfectnyke@gmail.com>
2023-08-28 17:05:42 -0700

    fixes pycodestyle

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,6 @@
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
 
-
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
@@ -13,5 +12,4 @@ def safe_print_list_integers(my_list=[], x=0):
 
 
     print()
-
     return printed
