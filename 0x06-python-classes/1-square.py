d45eaa43e3a4e57528463c9c8a0926554aeeac88
Ade-nike17 <perfectnyke@gmail.com>
2023-08-30 17:07:06 -0700

    module documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""This module contains a square class"""
+
 
 class Square:
     """This is a class rhat represents a square"""
     def __init__(self, size):
-        """Instantiation of method with size atteibute"""
+        """Instantiation of method with size attribute"""
         self.__size = size
