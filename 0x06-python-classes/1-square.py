c149597c0db74df5e288bade84de58154f0a5617
Ade-nike17 <perfectnyke@gmail.com>
2023-08-30 17:02:10 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Square:
+    """This is a class rhat represents a square"""
+    def __init__(self, size):
+        """Instantiation of method with size atteibute"""
+        self.__size = size
