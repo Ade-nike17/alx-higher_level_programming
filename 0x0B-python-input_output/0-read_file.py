83c57222de23060e9464f3c043b2cca1dc82a826
Ade-nike17 <perfectnyke@gmail.com>
2023-09-12 13:35:14 -0700

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Contains the read_file function """
+
+
+def read_file(filename=""):
+    """ reads a textfile and prints to the stdout """
+    with open("my_file_0.txt", encoding="UTF-8") as f:
+        print(f.read())
