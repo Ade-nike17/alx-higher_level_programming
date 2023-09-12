7e8a121384e96cb708d2546516a5d9d608709889
Ade-nike17 <perfectnyke@gmail.com>
2023-09-12 13:55:49 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ reads a textfile and prints to the stdout """
     with open("my_file_0.txt", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
