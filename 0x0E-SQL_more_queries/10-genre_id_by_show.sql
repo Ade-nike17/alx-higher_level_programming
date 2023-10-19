3606fbbd172ff4fe6f6fe9ebfcfadda858c3e761
Ade-nike17 <perfectnyke@gmail.com>
2023-10-19 11:27:06 -0700

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- Each record displays: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title, tv_shows_genres.genre_id
-FROM tv_shows_genres
-JOIN tv_shows ON tv_shows_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_shows_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
