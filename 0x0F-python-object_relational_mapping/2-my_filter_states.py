2b180b2e3dc7d5eec58170404edf2d080790dfc4
Ade-nike17 <perfectnyke@gmail.com>
2023-11-20 12:33:48 -0800

    comment

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-This script that lists all states from the database hbtn_0e_0_usa
+This script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 import MySQLdb
