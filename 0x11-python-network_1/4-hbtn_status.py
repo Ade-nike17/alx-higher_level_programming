5a50134d1edd4108472cfbc91c3edad55831f7d6
nyke <perfectnyke@gmail.com>
2023-12-27 15:04:08 +0100

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status
+"""
+
+import requests
+
+
+def fetch_and_display_status(url):
+    # Send a GET request to the url
+    response = requests.get(url)
+
+    # Display the body of the response with tabulation
+    print(f"Body response:")
+    print(f"\t- type : {type(response.text)}")
+    print(f"\t- content: {response.text}")
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    fetch_and_display_status(url)
