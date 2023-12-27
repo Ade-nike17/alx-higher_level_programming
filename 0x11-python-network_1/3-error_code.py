187ec3d4f4a1bd1f97599197774c59a289447d84
nyke <perfectnyke@gmail.com>
2023-12-27 14:40:24 +0100

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)
+"""
+
+import urllib.error
+import urllib.request
+import sys
+
+
+def send_request_and_display_body(url):
+    try:
+        # Send a request to the url
+        with urllib.request.urlopen(url) as response:
+            # Read and decode the response body
+            response_body = response.read().decode('utf-8')
+
+            # Display the body of the response
+            print(response_body)
+
+    except urllib.error.HTTPError as e:
+        print(f"Errror code: {e.code}")
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    send_request_and_display_body(url)
