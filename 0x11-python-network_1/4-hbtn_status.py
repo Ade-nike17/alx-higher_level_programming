9d5374db8b5ff0f33f99a266fe1f322476dcfaaa
nyke <perfectnyke@gmail.com>
2023-12-27 15:19:59 +0100

    error handling

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,13 +7,17 @@ import requests
 
 
 def fetch_and_display_status(url):
-    # Send a GET request to the url
-    response = requests.get(url)
+    try:
+        # Send a GET request to the url
+        response = requests.get(url)
 
-    # Display the body of the response with tabulation
-    print(f"Body response:")
-    print(f"\t- type : {type(response.text)}")
-    print(f"\t- content: {response.text}")
+        # Display the body of the response with tabulation
+        print(f"Body response:")
+        print(f"\t- type : {type(response.text)}")
+        print(f"\t- content: {response.text}")
+
+    except requests.RequestException as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
