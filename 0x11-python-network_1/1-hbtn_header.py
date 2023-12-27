fbf360c5e0b2392db9bb3bec21d503b53023e5c8
nyke <perfectnyke@gmail.com>
2023-12-27 11:50:11 +0100

    fixes pep-8 validation

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-""" This script takes in a URL
+"""
+This script takes in a URL
 Sends a request to the URL
-Displays the value of the X-Request-Id variable found in the header of the response"""
+Displays the value of the X-Request-Id variable
+"""
 import urllib.request
 import sys
 
+
 def get_x_request_id(url):
     # Send a request to the url
-        with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(url) as response:
         # Retrieve the X-Request-Id from the header
-            x_request_id = response.headers.get('X-Request-Id')
+        x_request_id = response.headers.get('X-Request-Id')
 
-            # Display the value of X-Request-Id
-            print(f"{x_request_id}")
+        # Display the value of X-Request-Id
+        print(f"{x_request_id}")
+
 
 if __name__ == "__main__":
     # Check if a URL is provided as a cmd-line arg
