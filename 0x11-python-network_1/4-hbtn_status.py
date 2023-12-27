3600c48593f0da8badebcfb5c325219e4d8f3f56
nyke <perfectnyke@gmail.com>
2023-12-27 15:27:19 +0100

    chars error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,17 +7,13 @@ import requests
 
 
 def fetch_and_display_status(url):
-    try:
-        # Send a GET request to the url
-        response = requests.get(url)
+    # Send a GET request to the url
+    response = requests.get(url)
 
-        # Display the body of the response with tabulation
-        print(f"Body response:")
-        print(f"\t- type : {type(response.text)}")
-        print(f"\t- content: {response.text}")
-
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+    # Display the body of the response with tabulation
+    print(f"Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
 
 
 if __name__ == "__main__":
